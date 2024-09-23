5d0118727f43d7461018f49f79a51f7392b33f4f
DergNoises <roninthewolf4@gmail.com>
2024-09-23 13:59:14 -0400

    new logger message

--- src/main/java/derg/projectivemagistry/ProjectiveMagistry.java
+++ src/main/java/derg/projectivemagistry/ProjectiveMagistry.java
@@ -23,6 +23,7 @@ public class ProjectiveMagistry implements ModInitializer {
 		// Proceed with mild caution.
 		ModItemGroups.registerItemGroups();
 		ModItems.initialize();
+		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
 		LOGGER.info("Loading Projective Magistry!");
 	}
