0fcd3a9d10b5f819b5856c67dfc7fc9f85811055
timvast <timothymcgee02@gmail.com>
2024-10-25 21:07:23 -0400

    fire wooo

--- src/main/java/derg/projectivemagistry/ProjectiveMagistry.java
+++ src/main/java/derg/projectivemagistry/ProjectiveMagistry.java
@@ -1,5 +1,6 @@
 package derg.projectivemagistry;
 
+import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
 import derg.projectivemagistry.common.block.ModBlocks;
 import derg.projectivemagistry.common.registry.ModItemGroups;
 import derg.projectivemagistry.common.item.ModItems;
@@ -29,5 +30,34 @@ public class ProjectiveMagistry implements ModInitializer {
 	public static Identifier id(String path) {
 		return Identifier.of(MOD_ID, path);
 	}
+	private static void registerFlammables() {
+		FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.ASH_LOG, 5, 5);
+		FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.ASH_WOOD, 5, 5);
 
+
+		FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.ASH_PLANKS, 5, 20);
+		FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.ASH_LEAVES, 30, 60);
+
+
+		FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.HOLLY_LOG, 5, 5);
+		FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.HOLLY_WOOD, 5, 5);
+
+
+		FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.HOLLY_PLANKS, 5, 20);
+		FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.HOLLY_LEAVES, 30, 60);
+
+		FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.HAZEL_LOG, 5, 5);
+		FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.HAZEL_WOOD, 5, 5);
+
+
+		FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.HAZEL_PLANKS, 5, 20);
+		FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.HAZEL_LEAVES, 30, 60);
+
+		FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.YEW_LOG, 5, 5);
+		FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.YEW_WOOD, 5, 5);
+
+
+		FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.YEW_PLANKS, 5, 20);
+		FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.YEW_LEAVES, 30, 60);
+	}
 }
