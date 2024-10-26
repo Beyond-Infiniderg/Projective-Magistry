5a2fb8ca6f1b563734423c16706f2397f6da62e0
timvast <timothymcgee02@gmail.com>
2024-10-26 14:31:41 -0400

    you get this ?

--- src/main/java/derg/projectivemagistry/common/world/tree/ModSaplingGenerators.java
+++ src/main/java/derg/projectivemagistry/common/world/tree/ModSaplingGenerators.java
@@ -11,11 +11,11 @@ public class ModSaplingGenerators {
             Optional.empty(), Optional.of(ModConfiguredFeatures.ASH_KEY), Optional.empty());
 
     public static final SaplingGenerator HAZEL = new SaplingGenerator(ProjectiveMagistry.MOD_ID + ":hazel",
-            Optional.empty(), Optional.of(ModConfiguredFeatures.ASH_KEY), Optional.empty());
+            Optional.empty(), Optional.of(ModConfiguredFeatures.HAZEL_KEY), Optional.empty());
 
     public static final SaplingGenerator HOLLY = new SaplingGenerator(ProjectiveMagistry.MOD_ID + ":holly",
-            Optional.empty(), Optional.of(ModConfiguredFeatures.ASH_KEY), Optional.empty());
+            Optional.empty(), Optional.of(ModConfiguredFeatures.HOLLY_KEY), Optional.empty());
 
     public static final SaplingGenerator YEW = new SaplingGenerator(ProjectiveMagistry.MOD_ID + ":yew",
-            Optional.empty(), Optional.of(ModConfiguredFeatures.ASH_KEY), Optional.empty());
+            Optional.empty(), Optional.of(ModConfiguredFeatures.YEW_KEY), Optional.empty());
 }
